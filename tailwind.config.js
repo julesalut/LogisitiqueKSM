64757526fe1afac6d949a64f3be26fd05ba15dad
julesalut <julesalut@icloud.com>
2024-11-18 21:39:22 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {},
   },
+  safelist: [
+    'bg-yellow-500',
+    'bg-orange-500',
+    'bg-green-500',
+    'bg-pink-500',
+    'bg-gray-100',
+    'bg-blue-500',
+    'text-blue-500',
+    'hover:bg-blue-600'
+  ],
   plugins: [],
 }
